ac06be1987c9f3cd245b36faa07339885b5f1ebc
Wmoreira-m <weslley.leli346@gmail.com>
2024-04-10 21:20:07 -0300

    Aula 6

---
+++ Aula06/src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+       String churros = "Preto";
+       String churros2 = "preto";
+
+       if (churros.equalsIgnoreCase(churros2)){
+           System.out.println("Churros iguais");
+       }
+       else {
+           System.out.println("Churros diferentes");
+       }
+
+
+
+
+
+
+        }
+    }
